937f4f10bd5073d98996b0ab47dac11cf494e554
Pablo Rodríguez <is.pab.ces@gmail.com>
2019-10-12 00:08:27 -0500

    First update
    First update

---
+++ trydjango/products/models.py
@@ -0,0 +1,9 @@
+from django.db import models
+
+
+class Product(models.Model):
+    title       = models.TextField()
+    description = models.TextField()
+    price       = models.TextField()
+    summary     = models.TextField(default='This is cool')
+
